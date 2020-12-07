6907eefa461552dbd0f38e176f6d89d96d9503d8
Luka <mbp@MacBook-Pro-MBP.local>
2020-12-07 18:09:15 +0200

    feat: added upload routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,18 +1,66 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
+
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/.(jpg|jpeg|png)$/)){
+            cb(new Error('Please upload a jpg, jpeg or png file'))
+        }
+        cb(undefined, true)
+    }
+})
 
-router.delete('/users/me', auth, async(req, res) => {
-    const { _id } = req.user
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (err, req, res, next) => {
+    res.status(400).send({error: err.message})
+})
+
+router.delete('/users/me/avatar', auth, async(req, res) => {
     try {
         await req.user.remove()
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
+})
 
+
+router.get('/users/:id/avatar', auth, async(req, res) => {
+    const { _id } = req.user
+    try {
+        const user = User.findById(_id)
+         if(!user || !user.avatar) {
+             throw new Error('')
+         }
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
+
+router.delete('/users/me', auth, async(req, res) => {
+    req.user.avatar = undefined
+    try {
+        await user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 router.post('/users', async (req, res) => {
@@ -26,7 +74,6 @@ router.post('/users', async (req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
-   
 
 })
 
